8061605bb372ac46325fa41f220832619ccd9233
Alexandre Cesar <huijari@users.noreply.github.com>
2019-10-31 17:53:39 -0300

    format code

--- index.js
+++ index.js
@@ -5,21 +5,25 @@ const imouto = require('./src/imouto')
 
 const bot = new Telegram(process.env.BOT_KEY)
 
-bot.getMe().then((bot_informations) => {
-    console.log("Server has initialized bot nickname. Nick: "+bot_informations.username)
+bot.getMe().then(bot_informations => {
+  console.log(
+    'Server has initialized bot nickname. Nick: ' + bot_informations.username
+  )
 })
 
 bot.setWebhook(process.env.ORIGIN)
 
 async function handler(request, response) {
-    const { inline_query } = await json(request)
-    if (inline_query && inline_query.query) {
-        let offset = inline_query.offset ? parseInt(inline_query.offset) : 1
-        const results = await imouto(offset, inline_query.query)
-        bot.answerInlineQuery(inline_query.id, results, {"next_offset": (offset+1).toString()})
-    }
+  const { inline_query } = await json(request)
+  if (inline_query && inline_query.query) {
+    let offset = inline_query.offset ? parseInt(inline_query.offset) : 1
+    const results = await imouto(offset, inline_query.query)
+    bot.answerInlineQuery(inline_query.id, results, {
+      next_offset: (offset + 1).toString()
+    })
+  }
 
-    return ''
+  return ''
 }
 
 module.exports = handler
--- src/imouto.js
+++ src/imouto.js
@@ -1,4 +1,4 @@
-const fetch = require('node-fetch');
+const fetch = require('node-fetch')
 
 const query = `
 query ($offset: Int, $title: String) { 
@@ -28,73 +28,95 @@ query ($offset: Int, $title: String) {
 `
 const url = 'https://graphql.anilist.co'
 
-function getMessageText(media) { 
-    return `<b>${media.title.english?media.title.english:media.title.native?media.title.native:media.title.romaji} ${(media.title.english||media.title.native)&&media.title.english!==media.title.romaji?"("+media.title.romaji+")":""}</b>
-<i>${media.genres.join(", ")}</i>
-${media.status} → ${media.type=='MANGA'?(media.chapters==null?'???':media.chapters):media.episodes} ${media.type=='MANGA'?'Chapters':'Episodes'}
-Mean Score: <b>${media.meanScore}</b> <a href="${media.coverImage.large}">(</a><a href="${media.siteUrl}">AniList)</a>
+function getMessageText(media) {
+  return `<b>${
+    media.title.english
+      ? media.title.english
+      : media.title.native
+      ? media.title.native
+      : media.title.romaji
+  } ${
+    (media.title.english || media.title.native) &&
+    media.title.english !== media.title.romaji
+      ? '(' + media.title.romaji + ')'
+      : ''
+  }</b>
+<i>${media.genres.join(', ')}</i>
+${media.status} → ${
+    media.type == 'MANGA'
+      ? media.chapters == null
+        ? '???'
+        : media.chapters
+      : media.episodes
+  } ${media.type == 'MANGA' ? 'Chapters' : 'Episodes'}
+Mean Score: <b>${media.meanScore}</b> <a href="${
+    media.coverImage.large
+  }">(</a><a href="${media.siteUrl}">AniList)</a>
 
 ${cleanHTMLFromText(media.description)}`
 }
 
 function cleanHTMLFromText(text) {
-    return text.replace(/<(?:.|\n)*?>/gm, '')
+  return text.replace(/<(?:.|\n)*?>/gm, '')
 }
 
 // Offset in inline query request is used as graphql query page
 async function getSearchResults(offset, title) {
-    let options = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-        },
-        body: JSON.stringify({
-            query: query,
-            variables: {offset, title}
-        })
-    }
+  let options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    },
+    body: JSON.stringify({
+      query: query,
+      variables: { offset, title }
+    })
+  }
 
-    try {
-        const response = await fetch(url, options)
-        const data = await handleAniListResponse(response)
-        const results = handleAniListResponseData(data)
-        return results
-    } catch(error) {
-        handleError(error)
-    }     
+  try {
+    const response = await fetch(url, options)
+    const data = await handleAniListResponse(response)
+    const results = handleAniListResponseData(data)
+    return results
+  } catch (error) {
+    handleError(error)
+  }
 }
 
 function handleAniListResponse(response) {
-    return response.json().then( (json) => {
-        return response.ok ? json : Promise.reject(json)
-    })
+  return response.json().then(json => {
+    return response.ok ? json : Promise.reject(json)
+  })
 }
 
 // Digest AniList response data, returning it as a Telegram inline query results object
 function handleAniListResponseData(data) {
-    let results = data.data.Page.SERIES.map((media) => {
-        return {
-            type: "article",
-            id: media.id,
-            title: `[${media.type}] ${media.title.english?media.title.english:media.title.romaji}`,
-            description: cleanHTMLFromText(media.description),
-            url: media.siteUrl,
-            hide_url: true,
-            thumb_url: media.coverImage.large,
-            thumb_width: 373,
-            thumb_height: 567,
-            input_message_content: {
-                message_text: getMessageText(media),
-                parse_mode: "HTML"
-            }
-        }
-    })
-    return results
+  let results = data.data.Page.SERIES.map(media => {
+    return {
+      type: 'article',
+      id: media.id,
+      title: `[${media.type}] ${
+        media.title.english ? media.title.english : media.title.romaji
+      }`,
+      description: cleanHTMLFromText(media.description),
+      url: media.siteUrl,
+      hide_url: true,
+      thumb_url: media.coverImage.large,
+      thumb_width: 373,
+      thumb_height: 567,
+      input_message_content: {
+        message_text: getMessageText(media),
+        parse_mode: 'HTML'
+      }
+    }
+  })
+  return results
 }
 
-function handleError(error) { // TODO
-    console.error(error)
+function handleError(error) {
+  // TODO
+  console.error(error)
 }
 
 module.exports = getSearchResults
