075b141382cad474cf7251b0b48d13c5c834523c
marscos <marcoscarvalhodeassis@gmail.com>
2022-09-30 20:51:20 -0300

    refactor: use destructuring assignment

--- api/index.js
+++ api/index.js
@@ -9,10 +9,11 @@ export default async (request, response) => {
   if (inline_query && inline_query.query) {
     let offset = inline_query.offset ? parseInt(inline_query.offset) : 1
     const results = await imouto(offset, inline_query.query)
-    const bot = new Telegraf(process.env.BOT_KEY).telegram
-    bot.telegram.answerInlineQuery(inline_query.id, results, {
+    const { telegram } = new Telegraf(process.env.BOT_KEY)
+    telegram.answerInlineQuery(inline_query.id, results, {
       next_offset: (offset + 1).toString()
     })
+
     return response.status(200).json(results)
   }
 
--- telegram.js
+++ telegram.js
@@ -1,9 +1,9 @@
 import Telegraf from 'telegraf'
 
-const bot = new Telegraf(process.env.BOT_KEY)
+const { telegram } = new Telegraf(process.env.BOT_KEY)
 
-bot.telegram.getMe().then(bot_informations => {
+telegram.getMe().then(bot_informations => {
   console.log(`${bot_informations.username} online`)
 })
 
-bot.telegram.setWebhook(process.env.ORIGIN)
+telegram.setWebhook(process.env.ORIGIN)
